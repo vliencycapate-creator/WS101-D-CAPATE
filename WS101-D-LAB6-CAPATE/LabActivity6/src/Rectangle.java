638be012e97a242e7e1a8d3b3d6cd804b8a40a53
vlinc <vliencycapateiii@gmail.com>
2025-11-17 13:36:56 +0800

    First

---
+++ WS101-D-LAB6-CAPATE/LabActivity6/src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    double length;
+    double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+}
